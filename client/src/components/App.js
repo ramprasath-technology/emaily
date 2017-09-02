3459fa5259d01831b0bd4fcc8b83db437559d5bb
Ramprasath Babu <ramprasath.technology@gmail.com>
2017-09-02 17:31:32 -0500

    creating priliminary styling using materialize.css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+
+const Landing = () => { return <div> Landing </div>}
+const Survey = () => { return <div> Survey </div>}
 
 const App = () => {
     return(
         <div>   
-            Hi There!
+            <BrowserRouter>          
+                <div>
+                    <Header />
+                    <Route path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Survey} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
